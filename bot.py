26655dc998945ff8e86c8488161de2e7daff1bd8
Sean O Neill <seanyoughal92@gmail.com>
2017-06-30 22:14:03 +0100

    Improved bot reading keyword

--- bot.py
+++ bot.py
@@ -14,17 +14,13 @@ tom_bot = praw.Reddit(user_agent=config.get('Bot', 'user_agent'),
 				      username=config.get('Bot', 'username'),
 				      password=config.get('Bot', 'password'))
 
-AllNewMessages = tom_bot.inbox.unread(limit=None)
- 
 for message in AllNewMessages:
-   if "irishfact" == message.body:
-	   what_comment = message.was_comment
-	   unread_messages = []
-	   if not what_comment:
-	   	print(message)
-	   	unread_messages.append(message)
-	   	tom_bot.inbox.mark_read(unread_messages)
-	   	what_user = str(message.author)
-	   	logging.info('Message sent to: ' +what_user)
-	   	mgs = random.choice(list(open('facts.txt')))
-	   	tom_bot.redditor(what_user).message('Ramdom Fact', 'Hello ' +what_user+',\n\n Here is your Irish ramdom fact: \n\n' +mgs)
+	   if "irishfact" in message.body.lower(): 
+	   	  unread_messages = []
+	   	  unread_messages.append(message)
+	   	  tom_bot.inbox.mark_read(unread_messages)
+	   	  what_user = str(message.author)
+	   	  logging.basicConfig(filename="simplelog.log",  format='%(asctime)s %(levelname)s %(message)s',level=logging.INFO)
+	   	  logging.info('Message sent to: ' +what_user)
+	   	  mgs = random.choice(list(open('facts.txt')))
+	   	  tom_bot.redditor(what_user).message('random Fact', 'Hello ' +what_user+',\n\n Here is your Irish random fact: \n\n' +mgs)
